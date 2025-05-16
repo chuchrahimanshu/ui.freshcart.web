db2b5bbdbfaeef58f3fe3459bf15f1f2a5e59ab8
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2025-05-17 01:32:35 +0530

    Create and Design ProductCategories section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import ProductCategories from "../components/ProductCategories";
 
 const Home = () => {
-  return <React.Fragment>hello</React.Fragment>;
+  return (
+    <React.Fragment>
+      <ProductCategories />
+    </React.Fragment>
+  );
 };
 
 export default Home;
